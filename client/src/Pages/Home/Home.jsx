cc7c19b802839f180978ff81fd86ea87eebeac8f
John Doe <mitka_j@yahoo.com>
2023-08-08 10:08:28 +0200

    events displayed in home page

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -13,27 +13,20 @@ const Home = () => {
 
   const navigate = useNavigate();
 
-  // Function to compare event dates for sorting
   const sortDate = (a, b) => {
     return new Date(a.eventDate) - new Date(b.eventDate);
   };
 
-  // Function to check if an event date has passed
   const isEventDatePassed = (eventDate) => {
     return dayjs(eventDate).isBefore(dayjs());
   };
 
   useEffect(() => {
     if (!isLoading && data) {
-      
-      // Create a copy of all events and sort them by date
       const allEvents = [...data.data].sort((a, b) => sortDate(a, b));
-
-      // Filter events that have not passed.
       const upcomingEvents = allEvents.filter((item) => !isEventDatePassed(item?.eventDate));
 
       if (upcomingEvents.length > 0) {
-        // Select a random event from upcomingEvents.
         const randomIndex = Math.floor(Math.random() * upcomingEvents.length);
         setRandomEvent(upcomingEvents[randomIndex]);
       }
@@ -54,6 +47,17 @@ const Home = () => {
     navigate("/musical-concerts");
   };
 
+  // Filter and display events
+  const upcomingConcerts = data?.data
+    ?.filter((item) => item?.eventType === "concert" && !isEventDatePassed(item?.eventDate))
+    .sort((a, b) => sortDate(a, b))
+    .slice(0, 5);
+
+  const upcomingComedies = data?.data
+    ?.filter((item) => item?.eventType === "comedy" && !isEventDatePassed(item?.eventDate))
+    .sort((a, b) => sortDate(a, b))
+    .slice(0, 5);
+
   return (
     <div>
       {randomEvent && <TopCard {...randomEvent} />}
@@ -61,28 +65,19 @@ const Home = () => {
       <div className="home__pageItems">
         <div className="home__pageItems--left">
           <h3 className="home__page__h3">Musical concerts</h3>
-          {/* Display the first 5 sorted concerts */}
-          {data?.data
-            ?.filter((item) => item?.eventType === "concert" && !isEventDatePassed(item?.eventDate))
-            .slice(0, 5)
-            .sort((a, b) => sortDate(a, b))
-            .map((item) => (
-              <Cards key={item._id} {...item} />
-            ))}
+          {upcomingConcerts?.map((item) => (
+            <Cards key={item._id} {...item} />
+          ))}
           <button className="home__page--exploreBtn" onClick={handleSeeAllConcerts}>
             See All
           </button>
         </div>
+
         <div className="home__pageItems--right">
           <h3 className="home__page__h3">Standup comedy</h3>
-          {/* Display the first 5 sorted comedies */}
-          {data?.data
-            ?.filter((item) => item?.eventType === "comedy" && !isEventDatePassed(item?.eventDate))
-            .slice(0, 5)
-            .sort((a, b) => sortDate(a, b))
-            .map((item) => (
-              <Cards key={item._id} {...item} />
-            ))}
+          {upcomingComedies?.map((item) => (
+            <Cards key={item._id} {...item} />
+          ))}
           <button className="home__page--exploreBtn" onClick={handleSeeAllComedies}>
             See All
           </button>
